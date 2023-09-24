3d32302840a30811b49c4297e40e0c5ed4ced5a0
Mauro Servienti <mauro.servienti@gmail.com>
2023-09-24 15:09:21 +0200

    Add basic tests for env var and config file support

--- src/NServiceBoXes.Endpoints.AmazonSQS.Tests/AmazonSqsEndpointTests.cs
+++ src/NServiceBoXes.Endpoints.AmazonSQS.Tests/AmazonSqsEndpointTests.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.Configuration;
 using NServiceBus;
 using NServiceBus.Configuration.AdvancedExtensibility;
 
@@ -6,10 +7,45 @@ namespace NServiceBoXes.Endpoints.AmazonSQS.Tests;
 public class AmazonSqsEndpointTests
 {
     [Fact]
-    public void Basic_endpoint_respect_name_and_default_values()
+    public void Basic_endpoint_respect_name()
     {
         var expectedEndpointName = "my-endpoint";
         var endpoint = new AmazonSqsEndpoint(expectedEndpointName);
+        EndpointConfiguration endpointConfiguration = endpoint;
+        
+        var actualEndpointName = endpointConfiguration.GetSettings().EndpointName();
+        
+        Assert.Equal(expectedEndpointName, actualEndpointName);
+    }
+    
+    [Fact]
+    public void Using_json_configuration_respect_name()
+    {
+        var expectedEndpointName = "my-endpoint";
+     
+        var config = new ConfigurationBuilder()
+            .AddJsonFile("endpoint.settings.json")
+            .Build();
+        
+        var endpoint = new AmazonSqsEndpoint(config);
+        EndpointConfiguration endpointConfiguration = endpoint;
+        
+        var actualEndpointName = endpointConfiguration.GetSettings().EndpointName();
+        
+        Assert.Equal(expectedEndpointName, actualEndpointName);
+    }
+    
+    [Fact]
+    public void Using_env_var_configuration_respect_name()
+    {
+        var expectedEndpointName = "my-endpoint";
+        Environment.SetEnvironmentVariable("NServiceBus:EndpointConfiguration:EndpointName", expectedEndpointName);
+        
+        var config = new ConfigurationBuilder()
+            .AddEnvironmentVariables()
+            .Build();
+        
+        var endpoint = new AmazonSqsEndpoint(config);
         EndpointConfiguration endpointConfiguration = endpoint;
         
         var actualEndpointName = endpointConfiguration.GetSettings().EndpointName();
