da09738b7a87823d72c006238a1a0f6266eea035
Mauro Servienti <mauro.servienti@gmail.com>
2023-09-24 14:01:51 +0200

    Snippets

---
+++ src/Snippets/BasicEndpoint.cs
@@ -0,0 +1,14 @@
+using NServiceBoXes.Endpoints.AmazonSQS;
+
+namespace Snippets;
+
+public class BasicEndpoint
+{
+    static async Task BasicEndpointUsage()
+    {
+        // begin-snippet BasicEndpointUsage
+        var endpoint = new AmazonSqsEndpoint("my-endpoint");
+        var endpointInstance = await endpoint.Start();
+        // end-snippet
+    }
+}
