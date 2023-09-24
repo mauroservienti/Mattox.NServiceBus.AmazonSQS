c345674468c1f276c5e75fe57e3f7bbb99b23cff
Mauro Servienti <mauro.servienti@gmail.com>
2023-09-24 21:16:43 +0200

    Remove tests performed in the parent repo and add transport configuration specific tests

--- src/NServiceBoXes.Endpoints.AmazonSQS.Tests/AmazonSqsEndpointTests.cs
+++ src/NServiceBoXes.Endpoints.AmazonSQS.Tests/AmazonSqsEndpointTests.cs
@@ -1,55 +1,23 @@
 using Microsoft.Extensions.Configuration;
-using NServiceBus;
-using NServiceBus.Configuration.AdvancedExtensibility;
 
 namespace NServiceBoXes.Endpoints.AmazonSQS.Tests;
 
 public class AmazonSqsEndpointTests
 {
     [Fact]
-    public void Basic_endpoint_respect_name()
+    public void When_using_json_config_to_set_name_prefixes()
     {
-        var expectedEndpointName = "my-endpoint";
-        var endpoint = new AmazonSqsEndpoint(expectedEndpointName);
-        EndpointConfiguration endpointConfiguration = endpoint;
+        var expectedQueueNamePrefix = "my-q";
+        var expectedTopicNamePrefix = "my-t";
         
-        var actualEndpointName = endpointConfiguration.GetSettings().EndpointName();
-        
-        Assert.Equal(expectedEndpointName, actualEndpointName);
-    }
-    
-    [Fact]
-    public void Using_json_configuration_respect_name()
-    {
-        var expectedEndpointName = "my-endpoint";
-     
-        var config = new ConfigurationBuilder()
-            .AddJsonFile("endpoint.settings.json")
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("When_using_json_config_to_set_name_prefixes.settings.json")
             .Build();
-        
-        var endpoint = new AmazonSqsEndpoint(config);
-        EndpointConfiguration endpointConfiguration = endpoint;
-        
-        var actualEndpointName = endpointConfiguration.GetSettings().EndpointName();
-        
-        Assert.Equal(expectedEndpointName, actualEndpointName);
-    }
-    
-    [Fact]
-    public void Using_env_var_configuration_respect_name()
-    {
-        var expectedEndpointName = "my-endpoint";
-        Environment.SetEnvironmentVariable("NServiceBus:EndpointConfiguration:EndpointName", expectedEndpointName);
-        
-        var config = new ConfigurationBuilder()
-            .AddEnvironmentVariables()
-            .Build();
-        
-        var endpoint = new AmazonSqsEndpoint(config);
-        EndpointConfiguration endpointConfiguration = endpoint;
-        
-        var actualEndpointName = endpointConfiguration.GetSettings().EndpointName();
-        
-        Assert.Equal(expectedEndpointName, actualEndpointName);
+
+        var endpoint = new AmazonSqsEndpoint(configuration);
+        var transportDefinition = endpoint.GetTransportDefinition();
+
+        Assert.Equal(expectedQueueNamePrefix, transportDefinition.QueueNamePrefix);
+        Assert.Equal(expectedTopicNamePrefix, transportDefinition.TopicNamePrefix);
     }
 }
