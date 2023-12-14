83361e5570c3d3ff5aa6c23decc050b4b2b8fa5d
Mauro Servienti <mauro.servienti@gmail.com>
2023-12-14 19:57:15 +0100

    Remove the need for the config manager

--- src/Maddox.NServiceBus.AmazonSQS/AmazonSqsEndpoint.cs
+++ src/Maddox.NServiceBus.AmazonSQS/AmazonSqsEndpoint.cs
@@ -5,7 +5,7 @@ using NServiceBus;
 
 namespace Maddox.NServiceBus.AmazonSQS;
 
-public class AmazonSqsEndpoint : NServiceBusEndpoint<AmazonSqsEndpointConfigurationManager, SqsTransport>
+public class AmazonSqsEndpoint : NServiceBusEndpoint<SqsTransport>
 {
     readonly IAmazonSQS? _amazonSqsClient;
     readonly IAmazonSimpleNotificationService? _amazonSnsClient;
@@ -25,6 +25,45 @@ public class AmazonSqsEndpoint : NServiceBusEndpoint<AmazonSqsEndpointConfigurat
         IAmazonSimpleNotificationService amazonSnsClient, IConfiguration? configuration = null)
         : base(endpointName, configuration)
     {
-        ConfigurationManager.InitializeClients(amazonSqsClient, amazonSnsClient);
+        _amazonSqsClient = amazonSqsClient ?? throw new ArgumentNullException(nameof(amazonSqsClient));
+        _amazonSnsClient = amazonSnsClient ?? throw new ArgumentNullException(nameof(amazonSnsClient));
+    }
+
+    protected override SqsTransport CreateTransport(IConfigurationSection? transportConfigurationSection)
+    {
+        SqsTransport transport;
+        if (_amazonSqsClient != null && _amazonSnsClient != null)
+        {
+            transport = new SqsTransport(_amazonSqsClient, _amazonSnsClient);
+        }
+        else
+        {
+            transport = new SqsTransport();
+        }
+
+        ApplyCommonTransportSettings(transportConfigurationSection, transport);
+        
+        transport.DoNotWrapOutgoingMessages = true;
+        if (transportConfigurationSection?["DoNotWrapOutgoingMessages"] is { } doNotWrapOutgoingMessages)
+        {
+            transport.DoNotWrapOutgoingMessages = bool.Parse(doNotWrapOutgoingMessages);
+        }
+
+        if (transportConfigurationSection?["QueueNamePrefix"] is { } queueNamePrefix)
+        {
+            transport.QueueNamePrefix = queueNamePrefix;
+        }
+
+        if (transportConfigurationSection?["TopicNamePrefix"] is { } topicNamePrefix)
+        {
+            transport.TopicNamePrefix = topicNamePrefix;
+        }
+
+        if (transportConfigurationSection?["MaxTimeToLive"] is { } maxTimeToLive)
+        {
+            transport.MaxTimeToLive = TimeSpan.Parse(maxTimeToLive);
+        }
+
+        return transport;
     }
 }
