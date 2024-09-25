6b8eba5ef2ca9a8bf908959eb5202a215b1e7a5b
Mauro Servienti <mauro.servienti@gmail.com>
2024-09-25 11:39:46 +0200

    Add more extension options

--- src/NServiceBoXes.Endpoints.AmazonSQS/HostBuilderExtensions.cs
+++ src/NServiceBoXes.Endpoints.AmazonSQS/HostBuilderExtensions.cs
@@ -5,11 +5,24 @@ namespace NServiceBoXes.Endpoints.AmazonSQS;
 
 public static class HostBuilderExtensions
 {
-    public static IHostBuilder UseNServiceBusAmazonSqsEndpoint(this IHostBuilder builder, Action<AmazonSqsEndpoint>? endpoint)
+    public static IHostBuilder UseNServiceBusAmazonSqsEndpoint(this IHostBuilder builder, Action<AmazonSqsEndpoint>? endpoint = null)
     {
         builder.UseNServiceBus(hostBuilderContext=>
         {
             var ep = new AmazonSqsEndpoint(hostBuilderContext.Configuration);
+            endpoint?.Invoke(ep);
+
+            return ep;
+        });
+        
+        return builder;
+    }
+    
+    public static IHostBuilder UseNServiceBusAmazonSqsEndpoint(this IHostBuilder builder, string endpointName, Action<AmazonSqsEndpoint>? endpoint = null)
+    {
+        builder.UseNServiceBus(hostBuilderContext=>
+        {
+            var ep = new AmazonSqsEndpoint(endpointName, hostBuilderContext.Configuration);
             endpoint?.Invoke(ep);
 
             return ep;
